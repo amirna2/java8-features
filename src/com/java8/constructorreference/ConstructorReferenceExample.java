2302295597916a7123b80bca163a5ea9cdf2c63c
Amir Nathoo <=amir.nth@gmail.com>
2019-10-26 17:11:28 -0700

    Minor update

--- src/com/java8/constructorreference/ConstructorReferenceExample.java
+++ src/com/java8/constructorreference/ConstructorReferenceExample.java
@@ -10,10 +10,9 @@ public class ConstructorReferenceExample {
     static Supplier<Student> studentSupplier = Student::new;
     static Function<String, Student> studentFunction = Student::new;
     
-    
     public static void main(String[] args) {
         System.out.println(studentSupplier.get());
-        System.out.println(studentFunction.apply("Bill"));
+        System.out.println(studentFunction.apply("Jphn"));
     }
 
 }
